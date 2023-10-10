09c689343abc58ff0b1f7b71c9f8a7860a48a08a
Karla <105371331+stakarla@users.noreply.github.com>
2023-10-10 07:39:29 -0300

    3

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play()  
+    document.querySelector('#som_tecla_pom').play()  ;
 }
 document.querySelector('#som_tecla_pom').onclick = tocaSomPom; 
