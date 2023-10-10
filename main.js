8031a907a839d2c7b72088ce03b992982a241509
Karla <105371331+stakarla@users.noreply.github.com>
2023-10-10 07:44:56 -0300

    ok

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play()  ;
 }
 document.querySelector('#som_tecla_pom').onclick = tocaSomPom; 
+
+function tocaSomClap(){
+    document.querySelector('#som_tecla_clap').play()  ;
+}
+document.querySelector('#som_tecla_clap).onclick = tocaSomClap; 
