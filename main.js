c46c51ad4d9e8e8157d9e0566da85943a5bccc09
Karla <105371331+stakarla@users.noreply.github.com>
2023-10-17 07:56:09 -0300

    while

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
+//criando/declarando a função tocasompom
 function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play()  ;
 }
+//chamando/invocando a função tocasompom
 document.querySelector('#som_tecla_pom').onclick = tocaSomPom; 
 
-function tocaSomClap(){
-    document.querySelector('#som_tecla_clap').play()  ;
-}
-document.querySelector('#som_tecla_clap).onclick = tocaSomClap; 
-
+//criando a referência constante litaDeTeclas
 const litaDeTeclas = document.querySelectorAll ('.tecla');
+//atribuindo o clique no botão 0 que é o primeiro item da lista de teclas
 litaDeTeclas[0].onclick = tocaSomPom
+
+//criando variavel contador, atribuindo o valor inicial como 0
+let contador = 0;
+//criando o código de repetição while
+while(){
+
+}
