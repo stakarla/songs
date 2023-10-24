ca2023d55bd63d4738d151d2d0d8f1ec18bc9d4b
Karla <105371331+stakarla@users.noreply.github.com>
2023-10-24 07:51:15 -0300

    1

--- main.js
+++ main.js
@@ -8,14 +8,14 @@ document.querySelector('.tecla_pom').onclick = tocaSom;
 //criando a referência constante litaDeTeclas
 const litaDeTeclas = document.querySelectorAll ('.tecla');
 //atribuindo o clique no botão 0 que é o primeiro item da lista de teclas
-litaDeTeclas[0].onclick = tocaSomPom
+litaDeTeclas[0].onclick = tocaSom
 
 //criando variavel contador, atribuindo o valor inicial como 0
 let contador = 0;
 //criando o código de repetição while
 //enquanto o contador for menos que o comprimento da lista tocara o som
 while(contador < listaDeTeclas.length){
-    listadeTeclas [contador].onclick = tocaSomPom;
+    listadeTeclas [contador].onclick = tocaSom;
     contador = contador + 1;
     console.log(contador);
 }
