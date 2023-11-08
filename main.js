1f8c76c4b6471d2410dce870d21aa7b77b7dbc67
Karla <105371331+stakarla@users.noreply.github.com>
2023-11-08 11:25:07 -0300

    1

--- main.js
+++ main.js
@@ -2,24 +2,21 @@
 function tocaSom(idElementoAudio){
     document.querySelector(idElementoAudio).play()  ;
 }
-//chamando/invocando a função tocasompom
-document.querySelector('.tecla_pom').onclick = tocaSom; 
 
 //criando a referência constante litaDeTeclas
 const litaDeTeclas = document.querySelectorAll ('.tecla');
-//atribuindo o clique no botão 0 que é o primeiro item da lista de teclas
-litaDeTeclas[0].onclick = tocaSom
 
 //criando variavel contador, atribuindo o valor inicial como 0
-let contador = 0;
+
+
 //criando o código de repetição while
 //enquanto o contador for menos que o comprimento da lista tocara o som
-while(contador < listaDeTeclas.length){
-    const efeito = listaDeTeclas[contador].classList[1]
-    const idAudio = "#som_" + efeito;
-    listadeTeclas [contador].onclick = function(){
+for(let contador = 0; contador < listaDeTeclas.length; contador = contador + 1;){
+    const tecla = listaDeTeclas[contador];
+    const efeito = tecla.classList[1]; //chama a classe do som
+    const idAudio = `#som_ ${efeito}`; //junção com template string
+    tecla.onclick = function(){ //função anônima para tocaSom
         tocaSom(idAudio);
     };
-    contador = contador + 1;
-    console.log(contador);
+       console.log(contador);
 }
