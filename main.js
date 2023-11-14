9e23e4283f307f2eacba9b62d9eea67959409337
Karla <105371331+stakarla@users.noreply.github.com>
2023-11-14 07:44:52 -0300

    virgula

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const litaDeTeclas = document.querySelectorAll ('.tecla');
 
 //criando o código de repetição while e mudando para for
 //enquanto o contador for menos que o comprimento da lista tocara o som
-for(let contador = 0; contador < listaDeTeclas.length; contador++;){
+for(let contador = 0; contador < listaDeTeclas.length; contador++){
     const tecla = listaDeTeclas[contador];
     const efeito = tecla.classList[1]; //chama a classe do som
     const idAudio = `#som_ ${efeito}`; //junção com template string
